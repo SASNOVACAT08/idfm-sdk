f6c8e20d576277bc3699e76313ea345885933e01
Ruben Barros <ruben.barr08@gmail.com>
2023-07-08 10:51:04 +0200

    test(env): add env for apiKey

--- packages/line-reports/test/index.test.ts
+++ packages/line-reports/test/index.test.ts
@@ -1,10 +1,12 @@
 import { describe, it, expect } from 'vitest';
 import { Api } from '../src/api';
 
+const { API_KEY } = process.env;
+
 describe('test', () => {
   it('should be ok', async () => {
-    const api = new Api('');
+    const api = new Api(API_KEY || '');
     const data = await api.getLineReports();
-    console.log(data); 
+    console.log(data.disruptions[0].messages); 
   });
 })
